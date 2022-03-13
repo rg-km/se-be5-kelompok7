bdc8dc9690fe4745627991067fc88dfdde25acf3
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-03-13 20:55:34 +0700

    slicing design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"]
+      }
+    },
   },
   plugins: [],
 }
